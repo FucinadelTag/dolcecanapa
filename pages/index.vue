b7b6e1cc62d11767c4dbe173144043ad62aa860e
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2018-05-04 18:54:34 +0200

    Ancora passi avanti

--- pages/index.vue
+++ pages/index.vue
@@ -7,77 +7,81 @@
         <div class="columns">
 
             <div class="column is-half">
-                <div class="box">
-                    <h2 class="title is-2">
-                        Cannabis Light Legale<br>
-                    </h2>
+                <a href="/landing/cannabis-legale">
+                    <div class="box">
+                        <h2 class="title is-2">
+                            Cannabis Light Legale<br>
+                        </h2>
 
-                    <div>
-                        <img v-bind:src="immagine_fumo.url" alt="" />
+                        <div>
+                            <img v-bind:src="immagine_fumo.url" alt="" />
+                        </div>
+                        <div class="spazio_bianco"></div>
+
+                        <div class="callToAction has-text-centered">
+                            <a class="button">Vedi le nostre Influorescenze</a>
+                        </div>
+
+                        <div class="spazio_bianco"></div>
+                        <p>
+                            <span class="is-size-5">Influorescenze ad uso tecnico:</span>
+
+                            <ul>
+                                <li>
+                                    HTC al di sotto dello 0,2% - <strong>Legale in italia</strong>
+                                </li>
+                                <li>
+                                    Influorescenze <strong>100% Italiane</strong>
+                                </li>
+                                <li>
+                                    Canapa biologica Certificata <strong>priva di metalli pesanti e pesticidi</strong>
+                                </li>
+                            </ul>
+                        </p>
+                        <p class="leggero">
+                            Non è un prodotto medicinale, alimentare o da combustione
+                        </p>
+
                     </div>
-                    <div class="spazio_bianco"></div>
-
-                    <div class="callToAction has-text-centered">
-                        <a class="button" href="/landing/cannabis-legale">Vedi le nostre Influorescenze</a>
-                    </div>
-
-                    <div class="spazio_bianco"></div>
-                    <p>
-                        <span class="is-size-5">Influorescenze ad uso tecnico:</span>
-
-                        <ul>
-                            <li>
-                                HTC al di sotto dello 0,2% - <strong>Legale in italia</strong>
-                            </li>
-                            <li>
-                                Influorescenze <strong>100% Italiane</strong>
-                            </li>
-                            <li>
-                                Canapa biologica Certificata <strong>priva di metalli pesanti e pesticidi</strong>
-                            </li>
-                        </ul>
-                    </p>
-                    <p class="leggero">
-                        Non è un prodotto medicinale, alimentare o da combustione
-                    </p>
-
-                </div>
+                </a>
 
             </div>
 
             <div class="column is-half">
-                <div class="box">
-                    <h2 class="title is-2">
-                        CBD: Olio e tanto altro
-                    </h2>
-                    <div>
-                        <img v-bind:src="immagine_olio.url" alt="" />
+                <a href="/landing/cbd">
+                    <div class="box">
+                        <h2 class="title is-2">
+                            CBD: Olio e tanto altro
+                        </h2>
+                        <div>
+                            <img v-bind:src="immagine_olio.url" alt="" />
+                        </div>
+                        <div class="spazio_bianco"></div>
+
+                        <div class="callToAction has-text-centered">
+                            <a class="button">Vedi i nostri prodotti con CBD</a>
+                        </div>
+
+                        <div class="spazio_bianco"></div>
+
+                        <p>
+                            <span class="is-size-5">Olio di Canapa puro ricco di Omega3 e Antiossidanti:</span>
+
+                            <ul>
+                                <li>Aiuta a ridurre <strong>dolori e infiammazioni</strong></li>
+                                <li>Dona freschezza e <strong>lucidità mentale</strong></li>
+                                <li><strong>Energizzante</strong>, capace di rigenerare energie e forze</li>
+                            </ul>
+                        </p>
+
+
+
+                        <p class="leggero">
+                            Non si tratta di un prodotto medicinale
+                        </p>
+
                     </div>
-                    <div class="spazio_bianco"></div>
-
-                    <div class="callToAction has-text-centered">
-                        <a class="button" href="/landing/cbd">Vedi i nostri prodotti con CBD</a>
-                    </div>
-
-                    <div class="spazio_bianco"></div>
-
-                    <p>
-                        <span class="is-size-5">Olio di Canapa puro ricco di Omega3 e Antiossidanti:</span>
-
-                        <ul>
-                            <li>Aiuta a ridurre <strong>dolori e infiammazioni</strong></li>
-                            <li>Dona freschezza e <strong>lucidità mentale</strong></li>
-                            <li><strong>Energizzante</strong>, capace di rigenerare energie e forze</li>
-                        </ul>
-                    </p>
-
-
-
-                    <p class="leggero">
-                        Non si tratta di un prodotto medicinale
-                    </p>
-
-                </div>
+                </a>
             </div>
 
         </div>
