d35660c4bdc824cf153845fb281af3c99bf5fc0e
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2018-05-09 17:41:26 +0200

    Inserita tabella caratteristiche

--- components/sliceCollection.vue
+++ components/sliceCollection.vue
@@ -19,6 +19,60 @@
                         <div class="callToAction">
                             <a href="#" class="button is-large" alt="Aggiungi al carrello"><i class="fas fa-cart-plus"></i>&nbsp;Aggiungi al carrello</a>
                         </div>
+                        <div class="spazio_bianco"></div>
+                        <table class="table is-bordered is-striped">
+                            <tbody>
+                                <tr v-if="prodotto.data.tipo_di_canapa.length > 0 && prodotto.data.tipo_di_canapa[0].text.length > 0">
+                                    <td style="width: 200px;">
+                                        <i>Tipo di canapa</i>
+                                    </td>
+                                    <th>
+                                        <span v-html="PrismicDom.RichText.asText(prodotto.data.tipo_di_canapa)"></span>
+                                    </th>
+                                </tr>
+
+                                <tr v-if="prodotto.data.percentuale_thc.length > 0 && prodotto.data.percentuale_thc[0].text.length > 0">
+                                    <td>
+                                        <i>Percentuale di THC</i>
+                                    </td>
+                                    <th>
+                                        <span v-html="PrismicDom.RichText.asText(prodotto.data.percentuale_thc)"></span>
+                                    </th>
+                                </tr>
+                                <tr v-if="prodotto.data.percentuale_cbd.length > 0 && prodotto.data.percentuale_cbd[0].text.length > 0">
+                                    <td>
+                                        <i>Percentuale di CBD</i>
+                                    </td>
+                                    <th>
+                                        <span v-html="PrismicDom.RichText.asText(prodotto.data.percentuale_cbd)"></span>
+                                    </th>
+                                </tr>
+                                <tr v-if="prodotto.data.quantita_contenuto.length > 0 && prodotto.data.quantita_contenuto[0].text.length > 0">
+                                    <td>
+                                        <i>Quantita</i>
+                                    </td>
+                                    <th>
+                                        <span v-html="PrismicDom.RichText.asText(prodotto.data.quantita_contenuto)"></span>
+                                    </th>
+                                </tr>
+
+                            </tbody>
+
+                            <!-- <div v-if="prodotto.data.tipo_di_canapa.length > 0 && prodotto.data.tipo_di_canapa[0].text.length > 0" >
+                                Tipo di canapa: <span v-html="PrismicDom.RichText.asText(prodotto.data.tipo_di_canapa)"></span>
+                            </div>
+                            <div v-if="prodotto.data.percentuale_thc.length > 0 && prodotto.data.percentuale_thc[0].text.length > 0" >
+                                Percentuale di THC: <span v-html="PrismicDom.RichText.asText(prodotto.data.percentuale_thc)"></span>
+                            </div>
+                            <div v-if="prodotto.data.percentuale_cbd.length > 0 && prodotto.data.percentuale_cbd[0].text.length > 0" >
+                                Percentuale di CBD: <span v-html="PrismicDom.RichText.asText(prodotto.data.percentuale_cbd)"></span>
+                            </div>
+                            <div v-if="prodotto.data.quantita_contenuto.length > 0 && prodotto.data.quantita_contenuto[0].text.length > 0" >
+                                Quantita: <span v-html="PrismicDom.RichText.asText(prodotto.data.quantita_contenuto)"></span>
+                            </div> -->
+                        </table>
+
+                        <div class="spazio_bianco"></div>
 
 
 
