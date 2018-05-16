c32332936cddbe61c20e4e50fe994123f48eac81
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2018-05-16 16:12:52 +0200

    Coretto numero verde

--- components/telefonoCallToAction.vue
+++ components/telefonoCallToAction.vue
@@ -2,7 +2,7 @@
     <section>
         <span class="is-size-2"> Ordina online e paga alla consegna</span>
         <div class="callToAction has-text-centered">
-            <a href="tel:800 800 800 800" class="button is-large" alt="800 800 800 800"> 800 800 800 800</a>
+            <a href="tel:800 800 800 800" class="button is-large" alt="800-180-439"> 800-180-439</a>
         </div>
         <span class="is-size-4"><i>Da lunedì a venerdì - dalle 9 alle 18</i></span>
 
