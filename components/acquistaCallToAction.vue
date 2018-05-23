2500bd8b00fae02a89bdc05f63bc98ace0b9becb
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2018-05-23 10:26:41 +0200

    Centrato bottone carrello

--- components/acquistaCallToAction.vue
+++ components/acquistaCallToAction.vue
@@ -1,6 +1,7 @@
 <template lang="html">
     <section>
         <span class="is-size-2"> Ordina online e paga alla consegna</span>
+        <p />
         <div class="callToAction has-text-centered">
             <a v-on:click="clickCallToAction()" href="#prodotti" class="button is-large" alt="Ordina adesso"> Ordina adesso</a>
         </div>
