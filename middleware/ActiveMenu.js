37732a2b12e7f5d9ba36a46d26b9bf9921f6acb3
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2018-06-06 17:00:08 +0200

    Piccoli fix sul menu

--- middleware/ActiveMenu.js
+++ middleware/ActiveMenu.js
@@ -1,6 +1,15 @@
 import _ from 'lodash'
 
 export default function ({ store, route }) {
-    //let firsLevel = _.split (route.path, "/", 3)[2];
-    store.commit('SET_ACTIVE_MENU', route.path)
+    let firsLevel = _.split (route.path, "/", 3)[1];
+
+    let path = '/'+firsLevel + '/'
+
+    if (firsLevel == 'landing') {
+        path = route.path
+    }
+
+    console.log(path);
+
+    store.commit('SET_ACTIVE_MENU', path)
 }
